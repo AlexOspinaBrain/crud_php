0401ed351f4cfe6be7c3d48c16702c5162356965
Alex Ospina <42880132+AlexOspinaBrain@users.noreply.github.com>
2022-08-01 07:14:12 -0500

    Delete config.php

--- db/config.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$config = [
-    'DB_HOST' => 'localhost',
-    'DB_USERNAME' => 'alex', // User for server
-    'DB_PASSWORD' => 'Mondongo23', // Password for server
-    'DB_DATABASE' => 'empleados', // Name database
-];
